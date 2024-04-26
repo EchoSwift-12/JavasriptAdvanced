d4ce9677d7504c43bc1c0bc69e70a375b7587b57
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2024-04-27 01:32:53 +0530

    Find, Filter function

--- OneDrive/Desktop/Repos/JavasriptAdvanced/app.js
+++ OneDrive/Desktop/Repos/JavasriptAdvanced/app.js
@@ -51,36 +51,89 @@
 // partyQueue.unshift("Richard")
 // console.log(partyQueue)// ['Richard', 'Charlie', 'David', 'Emily']
 
-let numbers = [1,2,3]
 
-const square=(num)=>{
-    return (num*num)
+// ------------map Function--------
+// let numbers = [1,2,3]
+
+// const square=(num)=>{
+//     return (num*num)
+// }
+// let sqarednumbers= numbers.map(square)
+
+// or---
+
+// let squarednumbers=numbers.map((num)=>{
+//     return num*num
+// })
+// console.log(squarednumbers)//[1, 4, 9]
+
+//  1.task 
+// let celTemp = [0,-40,100]
+
+// const celFehTemp= (temp)=>{
+//      return (temp*(9/5)+32)
+
+// }
+// let celFeh = celTemp.map(celFehTemp)
+// console.log(celFeh)
+
+// or---
+
+// let celTemp = [0,-40,100]
+
+// let fehTemp = celTemp.map((temp)=>{
+//     return (temp*(9/5)+32)
+// })
+
+// console.log(fehTemp)
+
+// 2.task
+
+// let celArr = [0, -40, 100]
+
+// const celtoObj = (cel) => {
+//     return ({
+//         c: cel,
+//         f: cel * (9 / 5) + 32
+//     })
+// }
+// console.log(celtoObj(0))//{c: 0, f: 32}
+
+
+
+// let resObj = celArr.map(celtoObj)
+// console.log(resObj)//// {c: 0, f: 32}
+// {c: -40, f: -40}
+// {c: 100, f: 212}
+
+
+
+// --------Find Function-------------
+
+// const isEven = (num) => {
+//     return num % 2 === 0
+// }
+
+// let numbers = [1, 2, 3, 4, 5]
+
+// let firstEvenNum = numbers.find(isEven)
+// console.log(firstEvenNum)//2
+
+// let evenNum = numbers.filter(isEven)
+// console.log(evenNum)//[2, 4]
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const threediv=(num)=>{
+    return num % 3===0
 }
-let sqarednumbers= numbers.map(square)
 
-or---
+let threeDivArr=arr.filter(threediv)
+console.log(threeDivArr)//[3, 6, 9]
 
-let squarednumbers=numbers.map((num)=>{
-    return num*num
-})
-console.log(squarednumbers)//[1, 4, 9]
+let threeArr=arr.find(threediv)
+console.log(threeArr)//3
 
 
-let celTemp = [0,-40,100]
 
-const celFehTemp= (temp)=>{
-     return (temp*(9/5)+32)
-    
-}
-let celFeh = celTemp.map(celFehTemp)
-console.log(celFeh)
-
-or---
-
-let celTemp = [0,-40,100]
-
-let fehTemp = celTemp.map((temp)=>{
-    return (temp*(9/5)+32)
-})
-
-console.log(fehTemp)
